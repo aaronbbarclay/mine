efae8ca89e11119f71b4547e25e146aa184d0360
Aaron Barclay <aaronb@snappy.gateway>
2019-09-01 10:04:43 +1000

    Adding RayOne

---
+++ RayOne/CMakeFiles/raymondo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raymondo.dir/src/exrWriteTest.cpp.o"
+  "raymondo.pdb"
+  "raymondo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raymondo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RayOne/src/exrWriteTest.h
@@ -0,0 +1,8 @@
+//
+// Created by Aaron Barclay on 31/8/19.
+//
+
+#ifndef RAYONE_EXRWRITETEST_H
+#define RAYONE_EXRWRITETEST_H
+
+#endif //RAYONE_EXRWRITETEST_H
